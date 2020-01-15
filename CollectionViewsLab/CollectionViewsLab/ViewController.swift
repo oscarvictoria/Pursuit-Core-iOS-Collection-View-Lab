d34923f0f072dedb57fca88b4f4e3c635f12dd13
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-01-15 18:03:18 -0500

    Created Project

---
+++ CollectionViewsLab/CollectionViewsLab/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CollectionViewsLab
+//
+//  Created by Oscar Victoria Gonzalez  on 1/15/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+@IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
